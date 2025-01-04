ff1e6c1c3404c9ba909bad582b4de6013bfee3a1
Nehan <nehanmohammed06@gmail.com>
2025-01-03 19:12:17 -0500

    rest of projects

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function closeModal(modalId) {
     }
 }
 
-document.getElementById('project1-trigger').addEventListener('click', () => showModal('project-modal'));
+document.getElementById('project1-trigger').addEventListener('click', () => showModal('project1-modal'));
 document.getElementById('project2-trigger').addEventListener('click', () => showModal('project2-modal'));
 document.getElementById('project3-trigger').addEventListener('click', () => showModal('project3-modal'));
 
@@ -60,14 +60,35 @@ function updateModalContent(modalId, title, description, images = []) {
     }
 }
 
+const carousels = document.querySelectorAll('.carousel');
+
+carousels.forEach((carousel) => {
+    const images = carousel.querySelector('.carousel-images');
+    const prevButton = carousel.querySelector('.carousel-button.prev');
+    const nextButton = carousel.querySelector('.carousel-button.next');
+    const imageCount = images.children.length;
+    let currentIndex = 0;
+
+    prevButton.addEventListener('click', () => {
+        currentIndex = (currentIndex - 1 + imageCount) % imageCount;
+        images.style.transform = `translateX(-${currentIndex * 100}%)`;
+    });
+
+    nextButton.addEventListener('click', () => {
+        currentIndex = (currentIndex + 1) % imageCount;
+        images.style.transform = `translateX(-${currentIndex * 100}%)`;
+    });
+});
+
+
 
 
 updateModalContent('project1-modal', '1P13 Project 1 - International Airport Challenge', 'Details about Project 1.', [
-    
+
 ]);
-updateModalContent('project2-modal', 'Project 2', 'Details about Project 2.', [
-    'project2-diagram.png'
+
+updateModalContent('project2-modal', 'Hand Gesture Controlled Rover', [
 ]);
-updateModalContent('project3-modal', 'Project 3', 'Details about Project 3.', [
-    'project3-diagram.png'
+
+updateModalContent('project3-modal', 'BOGOCHIB- Boid Simulation', [
 ]);
